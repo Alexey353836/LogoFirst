a953ceabf16350fc477e999eaee53713930817ed
Alexey353836 <127205134+Alexey353836@users.noreply.github.com>
2023-07-29 06:23:59 +0300

    сполер

--- js/logo.js
+++ js/logo.js
@@ -17,5 +17,15 @@ document.addEventListener('DOMContentLoaded', function() {
   var instances = M.Materialbox.init(elems);
 });
 
+// Карусель
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems);
+});
+
+// 
+var instance = M.Carousel.init({
+  fullWidth: true
+});
 
         
